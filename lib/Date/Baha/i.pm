cefa1561cc760d410220bbc96d326c7ed2b603be
Gene Boggs <gene.boggs@gmail.com>
2018-10-02 14:58:55 -0700

    Remove TO DO item

--- lib/Date/Baha/i.pm
+++ lib/Date/Baha/i.pm
@@ -805,8 +805,6 @@ L<http://calendar.bahaiq.com/>
 
 =head1 TO DO
 
-Re-create the missing 01-as_string.t, 03-misc.t & 04-to_bahai.t tests!
-
 Base the date computation on the time of day (Baha'i day begins at sunset).
 
 Make this a L<DateTime> module.
